eb3e6de0641a793ec4b18680d11041b2e9229fe0
Marcus Kvarnström <mk@bahnhof.se>
2012-07-19 22:06:23 +0200

    Learning domain

---
+++ Learning.Domain/Entities/Catalog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Learning.Domain.Entities
+{
+    class Catalog
+    {
+        public string CatalogID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Learning.Domain/Entities/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Learning.Domain.ValueObjects;
+
+namespace Learning.Domain.Entities
+{
+    class Course
+    {
+        public string CourseID { get; set; }
+        public string Name { get; set; }
+        public IList<Category> Categories { get; set; }
+    }
+}
